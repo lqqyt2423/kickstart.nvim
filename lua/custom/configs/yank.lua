291a528824a0c9fe850f5c5de82248d1dd1cc127
lqqyt2423 <974923609@qq.com>
2025-01-08 16:21:05 +0800

    YankDir YankFilename

--- lua/custom/configs/yank.lua
+++ lua/custom/configs/yank.lua
@@ -1,7 +1,17 @@
+local utils = require 'custom.utils'
+
+local function absolute_path()
+  return vim.fn.expand '%:p'
+end
+
+local function yank(str)
+  vim.fn.setreg('+', str)
+  vim.notify('Yank: ' .. str)
+end
+
 local function yank_path()
-  local path = vim.fn.expand '%:p'
-  vim.fn.setreg('+', path)
-  vim.notify('Yank: ' .. path)
+  local path = absolute_path()
+  yank(path)
 end
 
 vim.api.nvim_create_user_command('YankPath', function()
@@ -11,3 +21,31 @@ end, { desc = '[Y]ank [P]ath' })
 vim.keymap.set('n', '<leader>yp', function()
   yank_path()
 end, { desc = '[Y]ank [P]ath' })
+
+local function yank_dir()
+  local path = absolute_path()
+  local dir = utils.path_dir(path)
+  yank(dir)
+end
+
+vim.api.nvim_create_user_command('YankDir', function()
+  yank_dir()
+end, { desc = '[Y]ank [D]ir' })
+
+vim.keymap.set('n', '<leader>yd', function()
+  yank_dir()
+end, { desc = '[Y]ank [D]ir' })
+
+local function yank_filename()
+  local path = absolute_path()
+  local filename = utils.path_basename(path)
+  yank(filename)
+end
+
+vim.api.nvim_create_user_command('YankFilename', function()
+  yank_filename()
+end, { desc = '[Y]ank [F]ilename' })
+
+vim.keymap.set('n', '<leader>yf', function()
+  yank_filename()
+end, { desc = '[Y]ank [F]ilename' })
--- lua/custom/utils.lua
+++ lua/custom/utils.lua
@@ -14,4 +14,34 @@ function M.path_join(paths)
   return table.concat(vim.tbl_map(M.path_remove_trailing, paths), path_separator)
 end
 
+---@param path string
+---@return fun(): string
+function M.path_split(path)
+  return path:gmatch('[^' .. path_separator .. ']+' .. path_separator .. '?')
+end
+
+--- Get the basename of the given path.
+---@param path string
+---@return string
+function M.path_basename(path)
+  path = M.path_remove_trailing(path)
+  local i = path:match('^.*()' .. path_separator)
+  if not i then
+    return path
+  end
+  return path:sub(i + 1, #path)
+end
+
+--- Get the dir of the given path.
+---@param path string
+---@return string
+function M.path_dir(path)
+  path = M.path_remove_trailing(path)
+  local i = path:match('^.*()' .. path_separator)
+  if not i then
+    return path
+  end
+  return path:sub(1, i - 1)
+end
+
 return M
